ce22d825d5f0afe1792ca80cfa0d101ab34f5e66
Mathieu <mathieusoufflard@gmail.com>
2025-07-11 10:22:41 +0200

    Create place_data.dart
    this is juste the data of places

---
+++ map_favorite_place_app/lib/backend/data/place_data.dart
@@ -0,0 +1,10 @@
+import '../model/place.dart';
+
+final List<Place> places = [
+  Place(name: 'Tour Eiffel', category: 'Culturel', lat: 48.8584, lng: 2.2945),
+  Place(name: 'Musée du Louvre', category: 'Culturel', lat: 48.8606, lng: 2.3376),
+  Place(name: 'Cathédrale Notre-Dame de Paris', category: 'Religieux', lat: 48.8530, lng: 2.3499),
+  Place(name: 'Restaurant Le Jules Verne', category: 'Restauration', lat: 48.8579, lng: 2.2950),
+  Place(name: 'Arc de Triomphe', category: 'Culturel', lat: 48.8738, lng: 2.295),
+  Place(name: 'Galeries Lafayette Champs-Élysées', category: 'shopping', lat: 48.870706, lng: 2.306645),
+];
