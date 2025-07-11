6181807b8f0c64c1765ef87fb9f00974a0295390
Mathieu <mathieusoufflard@gmail.com>
2025-07-11 15:17:02 +0200

    Update home_page.dart
    divide the code in different function
    
    create fonction to : set the map _map(), set the market filter widget _markerFilter(), set the get the filter markers getFilteredMarkers(),
    
    update the ui of showPlaceInformationBottomSheet

--- map_favorite_place_app/lib/ui/page/home_page.dart
+++ map_favorite_place_app/lib/ui/page/home_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:map_favorite_place/ui/widgets/custom_image_network.dart';
 import 'package:map_favorite_place/ui/widgets/custom_text.dart';
-
 import '../../backend/data/place_data.dart';
 import '../../backend/model/place.dart';
 
@@ -18,91 +17,129 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   final mapController = MapController();
+  String selectedCategory = 'Tous';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: FlutterMap(
-            mapController: mapController,
-            options: MapOptions(
-              initialCenter: LatLng(48.8566, 2.3522),
-              initialZoom: 5.0,
-              maxZoom: 18,
-              minZoom: 4,
-            ),
+          child: Stack(
             children: [
-              TileLayer(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                userAgentPackageName: 'com.mapfavoriteplace.map_favorite_place',
-              ),
-              MarkerLayer(
-                markers: places.map((place) {
-                  return Marker(
-                    width: 40,
-                    height: 40,
-                    point: LatLng(place.lat, place.lng),
-                    child: GestureDetector(
-                      onTap: () {
-                        showPlaceInformationBottomSheet(context, place);
-                      },
-                      child: Icon(
-                        Icons.location_on,
-                        size: 40,
-                        color: Colors.red,
-                      ),
-                    ),
-                  );
-                }).toList(),
-              ),
+              _map(),
+              _markerFilter(),
             ],
-          ),
+          )
       ),
     );
   }
 
+  Widget _map() => FlutterMap(
+    mapController: mapController,
+    options: MapOptions(
+      initialCenter: LatLng(48.8566, 2.3522),
+      initialZoom: 5.0,
+      maxZoom: 18,
+      minZoom: 4,
+    ),
+    children: [
+      TileLayer(
+        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+        userAgentPackageName: 'com.mapfavoriteplace.map_favorite_place',
+      ),
+      MarkerLayer(
+        markers: getFilteredMarkers(),
+      ),
+    ],
+  );
+
+  Widget _markerFilter() => Positioned(
+    top: 10,
+    left: 10,
+    right: 10,
+    child: Center(
+      child: SegmentedButton<String>(
+        segments: [
+          ButtonSegment(value: 'Tous', label: CustomText.customText(text: 'Tous', color: Colors.black, fontSize: 13)),
+          ButtonSegment(value: 'Culturel', label: CustomText.customText(text: 'Culturel', color: Colors.black, fontSize: 13)),
+          ButtonSegment(value: 'Restauration', label: CustomText.customText(text: 'Restauration', color: Colors.black, fontSize: 13)),
+          ButtonSegment(value: 'shopping', label: CustomText.customText(text: 'shopping', color: Colors.black, fontSize: 13)),
+        ],
+        selected: {selectedCategory},
+        onSelectionChanged: (Set<String> value) {
+          setState(() {
+            selectedCategory = value.first;
+          });
+        },
+        multiSelectionEnabled: false,
+        showSelectedIcon: false,
+      ),
+    ),
+  );
+
+  List<Marker> getFilteredMarkers() {
+    final filteredPlaces = selectedCategory == 'Tous'
+        ? places
+        : places.where((place) => place.category == selectedCategory).toList();
+
+    return filteredPlaces.map((place) {
+      return Marker(
+        width: 40,
+        height: 40,
+        point: LatLng(place.lat, place.lng),
+        child: GestureDetector(
+          onTap: () => showPlaceInformationBottomSheet(context, place),
+          child: const Icon(
+            Icons.location_on,
+            size: 40,
+            color: Colors.red,
+          ),
+        ),
+      );
+    }).toList();
+  }
+
   void showPlaceInformationBottomSheet(BuildContext context, Place place) {
     showModalBottomSheet(
-      context: context,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-      ),
-      backgroundColor: Colors.white,
-      builder: (context) => SizedBox(
-        width: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                CustomImageNetwork(
-                  place.urlImage,
-                ),
-                SizedBox(height: 20),
-                CustomText.customText(
-                  text: place.name,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-                CustomText.customText(
-                  text: place.category,
-                  color: Colors.black,
-                  textAlign: TextAlign.left,
-                  fontSize: 15,
-                ),
-                CustomText.customText(
-                  text: place.description,
-                  color: Colors.black,
-                  fontSize: 17,
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-          )
+        context: context,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
         ),
-      )
+        backgroundColor: Colors.white,
+        builder: (context) => SizedBox(
+          width: double.infinity,
+          child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    CustomImageNetwork(
+                      place.urlImage,
+                    ),
+                    SizedBox(height: 20),
+                    CustomText.customText(
+                      text: place.name,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                    CustomText.customText(
+                      text: place.category,
+                      color: Colors.black,
+                      textAlign: TextAlign.left,
+                      fontSize: 15,
+                    ),
+                    CustomText.customText(
+                      text: place.description,
+                      color: Colors.black,
+                      fontSize: 17,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              )
+          ),
+        )
     );
   }
 }
