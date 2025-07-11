ac032829a56ce7be969232fc22891fab73f9a71d
Mathieu <mathieusoufflard@gmail.com>
2025-07-11 13:20:35 +0200

    Update place_data.dart
    add 2 places + refracto the alignement add set new attributes

--- map_favorite_place_app/lib/backend/data/place_data.dart
+++ map_favorite_place_app/lib/backend/data/place_data.dart
@@ -1,10 +1,52 @@
 import '../model/place.dart';
 
 final List<Place> places = [
-  Place(name: 'Tour Eiffel', category: 'Culturel', lat: 48.8584, lng: 2.2945),
-  Place(name: 'Musée du Louvre', category: 'Culturel', lat: 48.8606, lng: 2.3376),
-  Place(name: 'Cathédrale Notre-Dame de Paris', category: 'Religieux', lat: 48.8530, lng: 2.3499),
-  Place(name: 'Restaurant Le Jules Verne', category: 'Restauration', lat: 48.8579, lng: 2.2950),
-  Place(name: 'Arc de Triomphe', category: 'Culturel', lat: 48.8738, lng: 2.295),
-  Place(name: 'Galeries Lafayette Champs-Élysées', category: 'shopping', lat: 48.870706, lng: 2.306645),
+  Place(
+    name: 'Tour Eiffel',
+    category: 'Culturel',
+    lat: 48.8584,
+    lng: 2.2945,
+    description: 'Célèbre tour en fer de Gustave Eiffel (1889), terrasses panoramiques accessibles par escaliers et ascenseurs.',
+    urlImage: 'https://lh3.googleusercontent.com/gps-cs-s/AC9h4npY3tIRyfRIanafyTSKJkNduNqLHINm7YN6LiXEMk9_6HYaIATnkGougRwLMWKGiiOzxocWuUUJYVOWPtbOstBgKrXxjdYT-cnXZdE2Eg9CQeetpg2uhhC9ULUYEotDyZNGAWEDjzC1Vxs=w408-h544-k-no',
+  ),
+  Place(
+      name: 'Musée du Louvre',
+      category: 'Culturel',
+      lat: 48.8606,
+      lng: 2.3376,
+      description: 'Forteresse médiévale et résidence royale du XIVe siècle abritant aujourd\'hui le célèbre musée du Louvre.',
+      urlImage: 'https://lh3.googleusercontent.com/gps-cs-s/AC9h4nocJcQhbhNKvSj3CYgZklVERiPXNdsWyBB_FD40utSBSeB3AxEkCCIhMvTwMFI99SmwkPSQaqeCozH7-MXNNpUFCKZgyOXBbweO4iqf2xlDDn4jsBFNa1THYlSwE5O_rBmTCCl9=w408-h306-k-no',
+  ),
+  Place(
+      name: 'Cathédrale Notre-Dame de Paris',
+      category: 'Religieux',
+      lat: 48.8530,
+      lng: 2.3499,
+      description: 'Cathédrale colossale du XIIIe siècle ornée d\'arcs-boutants et de gargouilles, lieu du roman de Victor Hugo.',
+      urlImage: 'https://lh3.googleusercontent.com/gps-cs-s/AC9h4nrKP6HvfVGsHjWAW2XKD0a69xuvXkMugx_2XU7dEzuHd6pIQAqL-PxRZfsuwKtvtIly8-pPdHCNmhynxku2uKBR3-mGKFGJ2k6Hls6-xGnfRLXbDjpaDz83tW6yDt3v0RWdlIRh=w408-h544-k-no',
+  ),
+  Place(
+      name: 'Restaurant Le Jules Verne',
+      category: 'Restauration',
+      lat: 48.8579,
+      lng: 2.2950,
+      description: 'Restaurant clair et élégant dans la tour Eiffel, proposant une cuisine française créative et une vue sur la ville.',
+      urlImage: 'https://lh3.googleusercontent.com/p/AF1QipOZK_0hFCb82m77Vl_bKdDsiEmN83aPfyvRVncM=w408-h243-k-no',
+  ),
+  Place(
+      name: 'Arc de Triomphe',
+      category: 'Culturel',
+      lat: 48.8738,
+      lng: 2.295,
+      description: 'Arc de triomphe emblématique érigé pour commémorer les victoires de Napoléon, avec plateforme d\'observation.',
+      urlImage: 'https://lh3.googleusercontent.com/gps-cs-s/AC9h4nrj9omGm5TpwkvlBqNGqE6Muzcv74aun387p2HTejonECBRQNbO8KKszGo4tKEtCC3ZlqLz_oYW5jn-EOAHFRuh3yndKIO9zgzTDyU-NF1IxF1H5Wopq2TeGBaG5cN8SfLAh6Xq=w408-h544-k-no',
+  ),
+  Place(
+      name: 'Galeries Lafayette Champs-Élysées',
+      category: 'shopping',
+      lat: 48.870706,
+      lng: 2.306645,
+      description: 'Chaîne multimarque de prêt-à-porter (adultes et enfants), d’articles de beauté de luxe et de déco pour maison.',
+      urlImage: 'https://lh3.googleusercontent.com/p/AF1QipP8-aBU8VImWDjI_pKb3Q17OGPTaTEXrmmCUutJ=w426-h240-k-no',
+  ),
 ];
