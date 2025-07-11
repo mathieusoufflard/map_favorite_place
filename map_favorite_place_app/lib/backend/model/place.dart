82f58bb7d0783d2256da8116745d855317a337b0
Mathieu <mathieusoufflard@gmail.com>
2025-07-11 09:28:20 +0200

    Create place.dart
    create a class Place with name, category, lat (latitude) and lng (longitude)

---
+++ map_favorite_place_app/lib/backend/model/place.dart
@@ -0,0 +1,17 @@
+class Place{
+
+  final String name;
+
+  final String category;
+
+  final double lat;
+
+  final double lng;
+
+  Place({
+    required this.name,
+    required this.category,
+    required this.lat,
+    required this.lng,
+});
+}
