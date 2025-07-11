9d2fed8b7eea27c8822ab3d06638167a60bb9538
Mathieu <mathieusoufflard@gmail.com>
2025-07-11 15:27:51 +0200

    Update custom_text.dart
    change the class to a extends StatelessWidget class

--- map_favorite_place_app/lib/ui/widgets/custom_text.dart
+++ map_favorite_place_app/lib/ui/widgets/custom_text.dart
@@ -1,32 +1,25 @@
 import 'package:flutter/material.dart';
 
-/// Class for creating reusable customized text widgets.
-///
-/// This class provides a static method to generate a [Text] widget
-/// with configurable styling such as color, font size, alignment, etc.
-class CustomText {
+class CustomText extends StatelessWidget {
+  final String text;
+  final Color color;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final TextAlign? textAlign;
 
-  /// Private constructor to prevent instantiation.
-  CustomText._();
+  const CustomText({
+    super.key,
+    required this.text,
+    required this.color,
+    this.fontSize,
+    this.fontWeight,
+    this.textAlign,
+  });
 
-  /// Creates a custom-styled [Text] widget.
-  ///
-  /// - [text] : The text string to display.
-  /// - [color] : The color of the text.
-  /// - [fontSize] : The size of the font (optional).
-  /// - [fontWeight] : The weight of the font (optional).
-  /// - [textAlign] : The alignment of the text (optional).
-  ///
-  /// Returns a [Text] widget with the specified styling options.
-  static customText({
-    required String text,
-    required Color color,
-    double? fontSize,
-    FontWeight? fontWeight,
-    TextAlign? textAlign,
-  }) {
+  @override
+  Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsetsGeometry.symmetric(vertical: 10),
+      padding: const EdgeInsets.symmetric(vertical: 10),
       child: Text(
         text,
         textAlign: textAlign,
